b141fe7028c87f6e9d70290555b603209b47dabc
Gerry Gao <hologerry@gmail.com>
2018-08-29 17:01:35 +0800

    format files

--- wct.py
+++ wct.py
@@ -1,19 +1,13 @@
 from __future__ import division
 import torch
 from torch.utils.serialization import load_lua
-import torchvision.transforms as transforms
-import numpy as np
-import argparse
-import time
-import os
-from PIL import Image
-from modelsNIPS import decoder1,decoder2,decoder3,decoder4,decoder5
-from modelsNIPS import encoder1,encoder2,encoder3,encoder4,encoder5
+from modelsNIPS import decoder1, decoder2, decoder3, decoder4, decoder5
+from modelsNIPS import encoder1, encoder2, encoder3, encoder4, encoder5
 import torch.nn as nn
 
 
 class WCT(nn.Module):
-    def __init__(self,args):
+    def __init__(self, args):
         super(WCT, self).__init__()
         # load pre-trained network
         vgg1 = load_lua(args.vgg1)
@@ -27,7 +21,6 @@ class WCT(nn.Module):
         vgg5 = load_lua(args.vgg5)
         decoder5_torch = load_lua(args.decoder5)
 
-
         self.e1 = encoder1(vgg1)
         self.d1 = decoder1(decoder1_torch)
         self.e2 = encoder2(vgg2)
@@ -39,14 +32,14 @@ class WCT(nn.Module):
         self.e5 = encoder5(vgg5)
         self.d5 = decoder5(decoder5_torch)
 
-    def whiten_and_color(self,cF,sF):
+    def whiten_and_color(self, cF, sF):
         cFSize = cF.size()
-        c_mean = torch.mean(cF,1) # c x (h x w)
+        c_mean = torch.mean(cF, 1)  # c x (h x w)
         c_mean = c_mean.unsqueeze(1).expand_as(cF)
         cF = cF - c_mean
 
-        contentConv = torch.mm(cF,cF.t()).div(cFSize[1]-1) + torch.eye(cFSize[0]).double()
-        c_u,c_e,c_v = torch.svd(contentConv,some=False)
+        contentConv = torch.mm(cF, cF.t()).div(cFSize[1]-1) + torch.eye(cFSize[0]).double()
+        c_u, c_e, c_v = torch.svd(contentConv, some=False)
 
         k_c = cFSize[0]
         for i in range(cFSize[0]):
@@ -55,10 +48,10 @@ class WCT(nn.Module):
                 break
 
         sFSize = sF.size()
-        s_mean = torch.mean(sF,1)
+        s_mean = torch.mean(sF, 1)
         sF = sF - s_mean.unsqueeze(1).expand_as(sF)
-        styleConv = torch.mm(sF,sF.t()).div(sFSize[1]-1)
-        s_u,s_e,s_v = torch.svd(styleConv,some=False)
+        styleConv = torch.mm(sF, sF.t()).div(sFSize[1]-1)
+        s_u, s_e, s_v = torch.svd(styleConv, some=False)
 
         k_s = sFSize[0]
         for i in range(sFSize[0]):
@@ -67,24 +60,25 @@ class WCT(nn.Module):
                 break
 
         c_d = (c_e[0:k_c]).pow(-0.5)
-        step1 = torch.mm(c_v[:,0:k_c],torch.diag(c_d))
-        step2 = torch.mm(step1,(c_v[:,0:k_c].t()))
-        whiten_cF = torch.mm(step2,cF)
+        step1 = torch.mm(c_v[:, 0:k_c], torch.diag(c_d))
+        step2 = torch.mm(step1, (c_v[:, 0:k_c].t()))
+        whiten_cF = torch.mm(step2, cF)
 
         s_d = (s_e[0:k_s]).pow(0.5)
-        targetFeature = torch.mm(torch.mm(torch.mm(s_v[:,0:k_s],torch.diag(s_d)),(s_v[:,0:k_s].t())),whiten_cF)
+        targetFeature = torch.mm(torch.mm(torch.mm(s_v[:, 0:k_s], torch.diag(s_d)), (s_v[:, 0:k_s].t())), whiten_cF)
         targetFeature = targetFeature + s_mean.unsqueeze(1).expand_as(targetFeature)
         return targetFeature
 
-    def transform(self,cF,sF,csF,alpha):
+    def transform(self, cF, sF, csF, alpha):
         cF = cF.double()
         sF = sF.double()
-        C,W,H = cF.size(0),cF.size(1),cF.size(2)
-        _,W1,H1 = sF.size(0),sF.size(1),sF.size(2)
-        cFView = cF.view(C,-1)
-        sFView = sF.view(C,-1)
+        C = cF.size(0)
+        # C, W, H = cF.size(0), cF.size(1), cF.size(2)
+        # _, W1, H1 = sF.size(0), sF.size(1), sF.size(2)
+        cFView = cF.view(C, -1)
+        sFView = sF.view(C, -1)
 
-        targetFeature = self.whiten_and_color(cFView,sFView)
+        targetFeature = self.whiten_and_color(cFView, sFView)
         targetFeature = targetFeature.view_as(cF)
         ccsF = alpha * targetFeature + (1.0 - alpha) * cF
         ccsF = ccsF.float().unsqueeze(0)
