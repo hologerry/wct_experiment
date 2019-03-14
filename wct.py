890d885561dc8df8c4ae732aebd902aa838257e6
Gerry Gao <hologerry@gmail.com>
2019-03-14 20:32:14 +0800

    fix

--- wct.py
+++ wct.py
@@ -96,7 +96,7 @@ def style_transfer(args, content_img, style_img, imname, csF):
         wct = wct.cuda(args.gpu)
     sF5 = wct.e5(style_img)
     cF5 = wct.e5(content_img)
-    print("Feturemap: E5 size:", sF5.size())
+    # print("Feturemap: E5 size:", sF5.size())
     sF5 = sF5.data.cpu().squeeze(0)
     cF5 = cF5.data.cpu().squeeze(0)
     csF5 = wct.transform(cF5, sF5, csF, args.alpha)
@@ -104,7 +104,7 @@ def style_transfer(args, content_img, style_img, imname, csF):
 
     sF4 = wct.e4(style_img)
     cF4 = wct.e4(Im5)
-    print("Feturemap: E4 size:", sF4.size())
+    # print("Feturemap: E4 size:", sF4.size())
     sF4 = sF4.data.cpu().squeeze(0)
     cF4 = cF4.data.cpu().squeeze(0)
     csF4 = wct.transform(cF4, sF4, csF, args.alpha)
@@ -112,7 +112,7 @@ def style_transfer(args, content_img, style_img, imname, csF):
 
     sF3 = wct.e3(style_img)
     cF3 = wct.e3(Im4)
-    print("Feturemap: E3 size:", sF3.size())
+    # print("Feturemap: E3 size:", sF3.size())
     sF3 = sF3.data.cpu().squeeze(0)
     cF3 = cF3.data.cpu().squeeze(0)
     csF3 = wct.transform(cF3, sF3, csF, args.alpha)
@@ -120,7 +120,7 @@ def style_transfer(args, content_img, style_img, imname, csF):
 
     sF2 = wct.e2(style_img)
     cF2 = wct.e2(Im3)
-    print("Feturemap: E2 size:", sF2.size())
+    # print("Feturemap: E2 size:", sF2.size())
     sF2 = sF2.data.cpu().squeeze(0)
     cF2 = cF2.data.cpu().squeeze(0)
     csF2 = wct.transform(cF2, sF2, csF, args.alpha)
@@ -128,7 +128,7 @@ def style_transfer(args, content_img, style_img, imname, csF):
 
     sF1 = wct.e1(style_img)
     cF1 = wct.e1(Im2)
-    print("Feturemap: E1 size:", sF1.size())
+    # print("Feturemap: E1 size:", sF1.size())
     sF1 = sF1.data.cpu().squeeze(0)
     cF1 = cF1.data.cpu().squeeze(0)
     csF1 = wct.transform(cF1, sF1, csF, args.alpha)
